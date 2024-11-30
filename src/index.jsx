4ce437dc32b461abb612f736f57714d3d0188032
Jaimie <jaimie.j.hemmings@gmail.com>
2024-11-30 18:37:16 +0000

    add: better clouds

--- src/index.jsx
+++ src/index.jsx
@@ -24,5 +24,40 @@ root.render(
                 <Experience />
             </Canvas>
         </div>
+        <div className="wrapper">
+            <div className="container mx-auto">
+                <div className="section h-screen">
+                    <div className="container flex flex-col justify-around h-full p-5">
+                        <h1 className="text-center text-slate-900 text-5xl font-semibold">
+                        Lockheed Martin F-22 Raptor
+                        </h1>
+                        <p className="text-2xl text-slate-700 text-center md:w-[50%] mx-auto">
+                        The F-22 Raptor is a fifth-generation, stealth air superiority fighter known for its unmatched agility, advanced avionics, and dominant combat capabilities.
+                        </p>
+                    </div>
+                </div>
+                <div className="section h-screen flex flex-col justify-center">
+                    <div className="container">
+                        <h2 className="text-center text-slate-900 text-5xl font-semibold">
+                            Title Two
+                        </h2>
+                    </div>
+                </div>
+                <div className="section h-screen flex flex-col justify-center">
+                    <div className="container">
+                        <h2 className="text-center text-slate-900 text-5xl font-semibold">
+                            Title Three
+                        </h2>
+                    </div>
+                </div>
+                <div className="section h-screen flex flex-col justify-center">
+                    <div className="container">
+                        <h2 className="text-center text-slate-900 text-5xl font-semibold">
+                            Title Four
+                        </h2>
+                    </div>
+                </div>
+            </div>
+        </div>
     </>
 )
